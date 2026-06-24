6beaf823ef4e6ee1efea995de791df6015c8502d
khiem-commits <tintitxiem@gmail.com>
2026-06-24 14:17:05 +0700

    Restore gets() for password input in login function
    Re-enable unsafe gets() function for password input.

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-//const char* SLACK_API_TOKEN = "xoxb-123456789012-123456789012-A1B2C3D4E5F6G7H8I9J0K1L2"; 
+const char* SLACK_API_TOKEN = "xoxb-123456789012-123456789012-A1B2C3D4E5F6G7H8I9J0K1L2"; 
 
 void login() {
     char password[10];
     printf("Nhap mat khau he thong: ");
     
     // Sử dụng hàm nguy hiểm gets() gây lỗi tràn bộ đệm (Buffer Overflow)
-    //gets(password);
-    fgets(password, sizeof(password), stdin);
+    gets(password);
+    //fgets(password, sizeof(password), stdin);
 }
 
 int main() {

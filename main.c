f696b3cf036c8b04a028b3fa23027bafb6f137db
tintitxiem@gmail.com <tintitxiem@gmail.com>
2026-06-24 13:36:17 +0700

    rot

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 #include <string.h>
 
 // Cố tình tạo một API Key giả để làm mồi cho công cụ quét Secret
-const char* SLACK_API_TOKEN = "xoxb-123456789012-123456789012-A1B2C3D4E5F6G7H8I9J0K1L2"; 
+const char* SLACK_API_TOKEN = "DAY_LA_TOKEN_GIA_DE_DEMO_THAY_CO_OI";
 
 void login() {
     char password[10];
     printf("Nhap mat khau he thong: ");
     
-    // Sử dụng hàm nguy hiểm gets() gây lỗi tràn bộ đệm (Buffer Overflow)
+
     gets(password); 
 }
 

96831405321909ba3e5415d2c9508bdfb36831f1
khiem-commits <tintitxiem@gmail.com>
2026-06-24 14:06:54 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 // Cố tình tạo một API Key giả để làm mồi cho công cụ quét Secret
-const char* SLACK_API_TOKEN = "xoxb-123456789012-123456789012-A1B2C3D4E5F6G7H8I9J0K1L2"; 
+//const char* SLACK_API_TOKEN = "xoxb-123456789012-123456789012-A1B2C3D4E5F6G7H8I9J0K1L2"; 
 
 void login() {
     char password[10];
